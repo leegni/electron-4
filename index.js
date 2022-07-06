a84677ce5e339e67ec33be71509838edb4247007
Geunhee Lee <igni950424@gmail.com>
2022-07-06 16:55:34 +0900

    electron start

---
+++ index.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindows(){
+    let appWindow = new BrowserWindow({
+        width: 600,
+        height: 800,
+        center: true,
+        minWidth: 300     
+    });
+    appWindow.loadFile('./index.html');
+}
+
+app.on('ready', createWindows);
